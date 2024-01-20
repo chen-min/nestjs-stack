8f212901a3b58199103a3adf8db6ce1cc4911c50
chenmin <chenmin.unicloud.com>
2024-01-20 21:09:10 +0800

    update personal

--- src/app/(fatherRouter1)/about2/page.tsx
+++ src/app/(fatherRouter1)/about2/page.tsx
@@ -1,4 +1,25 @@
-function About() {
-  return <div className="min-h-screen"> 关于的页面2 </div>;
+async function About() {
+  async function getData() {
+    const response = await fetch(
+      "https://api-v2.xdclass.net/api/rank/v1/hot_product"
+    );
+
+    if (!response.ok) throw new Error("未找到");
+
+    const toJson = await response.json();
+
+    if (!toJson.data) throw new Error("404");
+
+    return toJson.data;
+  }
+
+  const data = await getData();
+  const title = data[0].productTitle;
+  return (
+    <div className="min-h-screen">
+      关于的页面2
+      <div>{title}</div>
+    </div>
+  );
 }
 export default About;
